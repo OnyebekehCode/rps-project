99b4b5c1d32a9f3bbdcb6a72c411e6ecb90ba554
Onyebekehcode <deemedx0@gmail.com>
2024-09-23 10:58:51 +0100

    change all instance of console.log() to alert()

--- index.js
+++ index.js
@@ -28,36 +28,36 @@ function playRound(humanChoice, computerChoice) {
     switch (humanChoice) {
         case "rock":
             if (computerChoice == "scissors") {
-                console.log("You win rock crushes scissors");
+                alert("You win rock crushes scissors");
                 humanScore++
             }else if (computerChoice == "paper") {
-                console.log("Computer win paper covers rock");
+                alert("Computer win paper covers rock");
                 computerScore++
-            }else {console.log(" it's a draw");
+            }else {alert(" it's a draw");
             }
             break;
         case "paper":
             if (computerChoice == "scissors") {
-                console.log("Computer win scissors cuts paper");
+                alert("Computer win scissors cuts paper");
                 computerScore++
             }else if (computerChoice == "rock") {
-                console.log("You win paper covers rock");
+                alert("You win paper covers rock");
                 humanScore++
-            }else {console.log("it's a draw");
+            }else {alert("it's a draw");
             }
             break;
         case "scissors":
             if (computerChoice == "paper") {
-                console.log("You win scissors cuts paper");
+                alert("You win scissors cuts paper");
                 humanScore++
             }else if (computerChoice == "rock") {
-                console.log("Computer win rock crushes scissors");
+                alert("Computer win rock crushes scissors");
                 computerScore++
-            }else {(console.log("it's a draw"));
+            }else {(alert("it's a draw"));
             }
             break;
             default:
-                console.log("............")
+                alert("............")
     }
     return {computerScore, humanScore}
 };
@@ -65,15 +65,15 @@ let humanSelection = getHumanChoice();
 let computerSelecton = getComputerChoice();
 
 playRound(humanSelection, computerSelecton);
-console.log(computerScore);
-console.log(humanScore);
+alert(computerScore);
+alert(humanScore);
 
 if (computerScore == 3) {
     alert("Computer gets the crown");
 }else if (humanScore >= 3) {
     alert("You're the Winner");
 }else {
-    console.log("Play on");
+    alert("Play on");
 };
 };
 playGame();
