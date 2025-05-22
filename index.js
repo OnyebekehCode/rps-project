38622355977c819685534dc5c72f90f7fdfbb52c
Samuel Onyebekeh <deemedx0@gmail.com>
2025-05-23 00:56:18 +0100

    updated UI and game logic

--- index.js
+++ index.js
@@ -1,158 +1,82 @@
-const computer = document.querySelector("#Computer");
-const Human =  document.querySelector("#human");
+const computerScoreD = document.querySelector("#Computer");
+const humanScoreD =  document.querySelector("#human");
 const button = document.querySelectorAll(".rock, .paper, .scissors");
-const display = document.querySelector("#ScoreBoard")
-const play = document.querySelector("#playButton")
-let computerScore = 0;
-let humanScore = 0;
-function getComputerChoice () {
-    let computerChoice = Math.floor(Math.random()*3 + 1);
-    if  (computerChoice <= 1) {
-        computerChoice = ("rock");
-    }else if (computerChoice <= 2) {
-        computerChoice = ("paper");
-    }else if (computerChoice <= 3){
-        computerChoice = ("scissors");
-    }else {
-        computerChoice = (" ");
-    };
-    return computerChoice
-};
- function getHumanChoice() {
-    if (event.target.className =="rock"|| event.target.className == "paper"|| event.target.className == "scissors") {
-        return event.target.className;
-    }else event.target.className = alert("input the correct word");
-};
-let humanChoice = getHumanChoice();
-for (i = 0; i < button.length; i++) {
-    button[i].addEventListener("click", function(event) {
-    humanChoice = getHumanChoice(event);
-})};
-
-play.addEventListener("click", function playRound(humanChoice, computerChoice) {
-
-    switch (humanChoice) {
-        case "rock":
-            if (computerChoice == "scissors") {
-                alert("You win rock crushes scissors");
-                humanScore++
-            }else if (computerChoice == "paper") {
-                alert("Computer win paper covers rock");
-                computerScore++
-            }else {alert(" it's a draw");
-            }
-            break;
-        case "paper":
-            if (computerChoice == "scissors") {
-                alert("Computer win scissors cuts paper");
-                computerScore++
-            }else if (computerChoice == "rock") {
-                alert("You win paper covers rock");
-                humanScore++
-            }else {alert("it's a draw");
-            }
-            break;
-        case "scissors":
-            if (computerChoice == "paper") {
-                alert("You win scissors cuts paper");
-                humanScore++
-            }else if (computerChoice == "rock") {
-                alert("Computer win rock crushes scissors");
-                computerScore++
-            }else {(alert("it's a draw"));
-            }
-            break;
-            default:
-                alert("............") 
-        };
-    return {computerScore, humanScore}
-    });
-
-
-
-/*
-function getComputerChoice () {
-    let computerChoice = Math.floor(Math.random()*3 + 1);
-    if  (computerChoice <= 1) {
-        computerChoice = ("rock");
-    }else if (computerChoice <= 2) {
-        computerChoice = ("paper");
-    }else if (computerChoice <= 3){
-        computerChoice = ("scissors");
-    }else {
-        computerChoice = (" ");
-    };
-    return computerChoice
-};
-
-function getHumanChoice() {
-    let humanChoice = prompt("write either rock, paper or scissors").toLowerCase();
-    if (humanChoice =="rock"|| humanChoice == "paper"|| humanChoice == "scissors") {
-        return humanChoice;
-    }else humanChoice = alert("input the correct word");
-};
+const display1 = document.querySelector("#Summary1");
+const display2 = document.querySelector("#Summary2");
 
 let computerScore = 0;
 let humanScore = 0;
 
-function playGame() {
+        for (let i = 0; i < button.length; i++) {
+         button[i].addEventListener("click", ()=>{
+            let humanChoice = button[i].classList[0];
+            let computerChoice = getComputerChoice();
+
+            playRound(humanChoice, computerChoice);
+
+            computerScoreD.textContent = computerScore;
+            humanScoreD.textContent = humanScore;
+
+            if (computerScore >= 5) {
+                display2.textContent = "Computer gets the crown";
+            }else if (humanScore >= 5) {
+                display2.textContent = "You\'re the Winner";
+            }else {
+                display2.textContent = "Play on";
+            };
+            })};
+
+    function getComputerChoice(){
+    let computerChoice = Math.floor(Math.random()* 3)
+      if  (computerChoice === 0) {
+        computerChoice = ("rock");
+    }else if (computerChoice === 1) {
+        computerChoice = ("paper");
+    }else if (computerChoice === 2){
+        computerChoice = ("scissors");
+    }
+    return computerChoice;
+};
+
+
 function playRound(humanChoice, computerChoice) {
 
+    if (humanChoice === computerChoice) {
+        display1.textContent = "It\'s a Draw"
+            return;
+    }
+
     switch (humanChoice) {
         case "rock":
-            if (computerChoice == "scissors") {
-                alert("You win rock crushes scissors");
+            if (computerChoice === "scissors") {
+                display1.textContent = "You win rock crushes scissors";
                 humanScore++
-            }else if (computerChoice == "paper") {
-                alert("Computer win paper covers rock");
+            }else if (computerChoice === "paper") {
+               display1.textContent = "Computer win paper covers rock";
                 computerScore++
-            }else {alert(" it's a draw");
             }
             break;
         case "paper":
-            if (computerChoice == "scissors") {
-                alert("Computer win scissors cuts paper");
+            if (computerChoice === "scissors") {
+                display1.textContent = "Computer win scissors cuts paper";
                 computerScore++
-            }else if (computerChoice == "rock") {
-                alert("You win paper covers rock");
+            }else if (computerChoice === "rock") {
+                display1.textContent = "You win paper covers rock";
                 humanScore++
-            }else {alert("it's a draw");
             }
             break;
         case "scissors":
-            if (computerChoice == "paper") {
-                alert("You win scissors cuts paper");
+            if (computerChoice === "paper") {
+                display1.textContent = "You win scissors cuts paper";
                 humanScore++
-            }else if (computerChoice == "rock") {
-                alert("Computer win rock crushes scissors");
+            }else if (computerChoice === "rock") {
+                display1.textContent = "Computer win rock crushes scissors";
                 computerScore++
-            }else {(alert("it's a draw"));
             }
             break;
             default:
-                alert("............")
-    }
-    return {computerScore, humanScore}
-};
-let humanSelection = getHumanChoice();
-let computerSelecton = getComputerChoice();
+                display1.textContent = "What\'s the hold?"}
 
-playRound(humanSelection, computerSelecton);
-alert(computerScore);
-alert(humanScore);
+};
 
-if (computerScore == 3) {
-    alert("Computer gets the crown");
-}else if (humanScore >= 3) {
-    alert("You're the Winner");
-}else {
-    alert("Play on");
-};
-};
-playGame();
-playGame();
-playGame();
-playGame();
-playGame();
-playGame();
-*/
+
